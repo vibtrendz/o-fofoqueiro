05d67a59f8b3b32048dd6f5c2244e491c6e4b8ad
vibtrendz <lieurkeneh1@gmail.com>
2025-05-12 13:08:14 -0500

    fixed txt files

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,5 +1,4 @@
-`use client`;
-
+"use client";
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
--- app/(admin)/posts/[id]/edit/page.tsx
+++ app/(admin)/posts/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-`use client`;
+"use client";
 
 import React, { useEffect, useState } from 'react';
 import { useParams, notFound } from 'next/navigation';
